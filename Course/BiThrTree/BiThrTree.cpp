501c3ae43687d88433042d2cebbb71863515108a
liaoyilan <2425494170@qq.com>
2021-04-11 20:51:03 +0800

    2020/4/11

--- Course/BiThrTree/BiThrTree.cpp
+++ Course/BiThrTree/BiThrTree.cpp
@@ -3,3 +3,55 @@
  * @Date: 2021-04-10 19:29:17
  * @Description: for my ying~
  */
+
+#include "BiThrTree.h"
+
+// - - - - - 二叉中序线索树操作的实现 - - - - -
+Status CreateBiThrTree(BiThrTree &T, char *path) //先序建立二叉树
+{
+    FILE *fp;
+    int ReadFromConsole;
+
+    ReadFromConsole = (path == NULL || strcmp(path, "") == 0);
+
+    if (ReadFromConsole)
+    {
+        printf("请输入二叉树的先序序列，如果没有子结点，使用^代替：");
+        CreateTree(T, NULL);
+    }
+    else
+    {
+        fp = fopen(path, "r");
+        if (fp == NULL)
+            return ERROR;
+        CreateTree(T, fp);
+        fclose(fp);
+    }
+    return OK;
+}
+
+// - - - - - 内部函数的实现 - - - - -
+Status CreateTree(BiThrTree &T, FILE *fp)
+{
+    char ch;
+
+    if (fp == NULL)
+        scanf("%c", &ch);
+    else
+    {
+        fscanf(fp, "%c", &ch);
+    }
+    if (ch == '^')
+        T = NULL;
+    else
+    {
+        T = (BiThrTree)malloc(sizeof(BiThrNode));
+        if (T == NULL)
+            exit(OVERFLOW);
+        T->data = ch;
+        CreateTree(T->lchild, fp); //创建左子树
+        CreateTree(T->rchild, fp); //创建右子树
+    }
+}
+
+// - - - - - 图形化输出实现 - - - - -
--- Course/BiThrTree/BiThrTree.h
+++ Course/BiThrTree/BiThrTree.h
@@ -4,7 +4,14 @@
  * @Description: for my ying~
  */
 
-// - - - - - 二叉树的二叉线索存储表示 - - - - -
+// - - - - - 二叉树的二叉中序线索存储表示 - - - - -
+#ifndef BITHRTREE_H
+#define BITHRTREE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../../Status/Status.h"
 
 //Link == 0:指针，Thread == 1:线索
 typedef enum PointerTag
@@ -12,8 +19,29 @@ typedef enum PointerTag
     Link,
     Thread
 };
+
 typedef char TElemType;
+
 typedef struct BiThrNode
 {
     TElemType data;
-};
+    struct BiThrNode *lchild, *rchild; //左右孩子指针
+    PointerTag LTag, RTag;             //左右标志
+} BiThrNode, *BiThrTree;
+
+// - - - - - 二叉树中序线索树的操作声明 - - - - -
+Status CreateBiThrTree(BiThrTree &T, char *path); //先序建立二叉树
+
+Status InOrderTraverse_Thr(BiThrTree &T, Status (*Visit)(TElemType));
+
+Status InorderThreading(BiThrTree &Thrt, BiThrTree T);
+
+// - - - - - 内部函数的声明 - - - - -
+Status CreateTree(BiThrTree &T, FILE *fp);
+
+// - - - - -图形化输出 - - - - -
+void PaintTree(BiThrTree &T);
+
+void PaintBiTree(TElemType **tmp, BiThrTree &T, int x, int y);
+
+#endif
